a963cc28dd2a813c557a6bcc4116555da2ec2b4e
unknown <joakig6@gmail.com>
2023-09-12 23:24:10 -0300

    try to fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import type { NextRequest } from "next/server";
 import Cookie from 'js-cookie'
 export async function middleware(req: NextRequest) {
   const token = Cookie.get("accessToken");
-
+  console.log(token)
   if (!token) {
+    console.log('no hay token')
     const resquestedPage = req.nextUrl.pathname; //ruta solicitada
     const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
     url.pathname = "/"; ///seteamos la url a donde redirigiremos
