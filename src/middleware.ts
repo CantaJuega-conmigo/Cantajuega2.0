157fbd922d50b30cadec21cac0c738cd3361b20a
unknown <joakig6@gmail.com>
2023-10-11 10:59:06 -0300

    probando

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("accesscookie");
-  console.log(token, "en middleware next");
-  if (!token) {
-    console.log("se ejecuto la condicional");
-    const resquestedPage = req.nextUrl.pathname; //ruta solicitada
-    const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
-    url.pathname = "/"; ///seteamos la url a donde redirigiremos
-    return NextResponse.redirect(url); //redirigimos a url base
+  try {
+    
+    const token = req.cookies.get("accesscookie");
+    console.log(token, "en middleware next");
+    if (!token) {
+      console.log("se ejecuto la condicional");
+      const resquestedPage = req.nextUrl.pathname; //ruta solicitada
+      const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
+      url.pathname = "/"; ///seteamos la url a donde redirigiremos
+      return NextResponse.redirect(url); //redirigimos a url base
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+    
   }
-  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
