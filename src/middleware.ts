5a6a46c6552c67c96568ff11ec2710028df1e2af
unknown <joakig6@gmail.com>
2023-10-11 12:13:33 -0300

    probando xddd

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  try {
-    
-    const token =  req.cookies.get("accesscookie");
-    console.log('explorando req',req.cookies.getAll()),
-    console.log(token, "en middleware next");
-    if (!token) {
-      console.log("se ejecuto la condicional");
-      const resquestedPage = req.nextUrl.pathname; //ruta solicitada
-      const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
-      url.pathname = "/"; ///seteamos la url a donde redirigiremos
-      return NextResponse.redirect(url); //redirigimos a url base
-    }
-    return NextResponse.next();
-  } catch (error) {
-    console.log(error);
-    
+  const token = req.cookies.get("accesscookie");
+  console.log('existe el token?',req.cookies.has('accesscookie'))
+  if (!token) {
+    console.log('no existe kpo anda pa ya');
+    const resquestedPage = req.nextUrl.pathname; //ruta solicitada
+    const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
+    url.pathname = "/"; ///seteamos la url a donde redirigiremos
+    return NextResponse.redirect(url); //redirigimos a url base
   }
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
