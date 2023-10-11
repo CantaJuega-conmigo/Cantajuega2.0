6a1d31865b47af30baf8019b2ecdbce4d600f633
unknown <joakig6@gmail.com>
2023-10-11 13:28:28 -0300

    some ux changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,21 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("accesscookie");
-  console.log('existe el token?',req.cookies.has('accesscookie'))
-  if (!token) {
-    console.log('no existe kpo anda pa ya');
-    const resquestedPage = req.nextUrl.pathname; //ruta solicitada
-    const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
-    url.pathname = "/"; ///seteamos la url a donde redirigiremos
-    return NextResponse.redirect(url); //redirigimos a url base
-  }
-  return NextResponse.next();
+// try {
+//     if (!token) {
+//       const resquestedPage = req.nextUrl.pathname; //ruta solicitada
+//       const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
+//       url.pathname = "/"; ///seteamos la url a donde redirigiremos
+//       return NextResponse.redirect(url); //redirigimos a url base
+//     }
+//     return NextResponse.next();
+  
+// } catch (error) {
+//   console.log(error)
+// }
+console.log('token',token);
+
+return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
