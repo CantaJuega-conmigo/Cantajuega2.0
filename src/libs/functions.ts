5cb3be98e220ff064e883fc106df27820ce8cbb7
Wild-Design <brenneke_ruger@hotmail.com>
2023-10-13 11:39:39 -0300

    Login completed

--- src/libs/functions.ts
+++ src/libs/functions.ts
@@ -1,14 +1,13 @@
-import axios from "./axios";
-import { setUser } from "@/store/userSlice";
-import { store } from "@/store/store";
-import Cookies from "js-cookie";
-import { loginResponse, loginbody, registerBody, responses } from "@/types";
-import { errorResponses } from "@/types/query/query.type.responses";
+import axios from './axios';
+import { setUser } from '@/store/userSlice';
+import { store } from '@/store/store';
+import Cookies from 'js-cookie';
+import { loginResponse, loginbody, registerBody, responses } from '@/types';
 
 export async function registerUser(body: registerBody): Promise<void> {
   try {
     const query: responses<loginResponse> = (
-      await axios.post("/user/register", body)
+      await axios.post('/user/register', body)
     ).data;
     const { user } = query.data![0];
     store.dispatch(setUser(user));
@@ -20,31 +19,40 @@ export async function registerUser(body: registerBody): Promise<void> {
 }
 
 export async function loginUser(body: loginbody): Promise<void> {
-  const isAnySession = Cookies.get("accesscookie");
-  fetch(`${process.env.NEXT_PUBLIC_API_URL}/cookie`, { credentials: "include" })
+  const isAnySession = Cookies.get('accesscookie');
+  fetch(`${process.env.NEXT_PUBLIC_API_URL}/cookie`, { credentials: 'include' })
     .then((res) => console.log(res))
     .then((err) => console.log(err));
   if (!isAnySession) {
     try {
       const petition: responses<loginResponse> = (
-        await axios.post("/user/login", body)
+        await axios.post('/user/login', body)
       ).data;
       const { user } = petition.data![0];
+      console.log(user);
+
       store.dispatch(setUser(user));
-      alert(`bienvenido ${user.firstName}`);
-      return;
-    } catch (error) {
-      console.log(error);
-      throw new Error("algo salio mal :(");
+    } catch (error: any) {
+      let message;
+      if (error.response.message) {
+        message = error.response.message;
+      } else if (typeof error.response.data.message === 'string') {
+        message = error.response.data.message;
+      } else if (error.response.data.message.errors[0].msg) {
+        message = error.response.data.message.errors[0].msg;
+      } else {
+        message = 'Ocurrió un error, intente mas tarde';
+      }
+      throw new Error(message);
     }
   } else {
-    throw new Error("Ya hay una sesion activa");
+    throw new Error('Ya hay una sesion activa');
   }
 }
 
 export async function logoutUser() {
   try {
-    const resquest = await axios.get("/user/logout");
+    const resquest = await axios.get('/user/logout');
     store.dispatch(setUser(null));
     alert(resquest.data);
   } catch (error) {
