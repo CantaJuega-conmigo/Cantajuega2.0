37df951d078c67e1c988253cd3f0afee135dce80
unknown <joakig6@gmail.com>
2023-10-14 00:30:54 -0300

    little change

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="min-h-screen bg-blue flex flex-col ">
-      <header className="w-full flex justify-center">Opciones de navegacion del admin</header>
+      <header className="w-full flex justify-center">Opciones de navegacion del admin <Link href={'/'} className="bg-white p-3">volver a home</Link></header>
       <main className="w-full">{children}</main>
     </div>
   );
