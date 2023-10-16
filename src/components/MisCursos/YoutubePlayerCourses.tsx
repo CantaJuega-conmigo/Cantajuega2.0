abb07067c1873b9b9d38303aa2c8744d44154f99
unknown <joakig6@gmail.com>
2023-10-15 22:21:52 -0300

    probando en produccion: progresos

--- src/components/MisCursos/YoutubePlayerCourses.tsx
+++ src/components/MisCursos/YoutubePlayerCourses.tsx
@@ -30,13 +30,17 @@ export default function YoutubePlayerCourses({
     playerVars: {},
     origin: "http://localhost:3000",
   };
+  console.log(Progress?.id, select, "para ver en produccion",ChildExists);
 
   const { data } = useGetProgressChildBySelectQuery(
     { ProgressId: Progress?.id!, select: select ?? videostypes.First_Video },
     {
-      selectFromResult: ({ data }) => ({
-        data:data?.data![0] //una vez recibida la data, la transformamos, y nos quedamos con la etapa del niño
-      }),
+      selectFromResult: ({ data }) => {
+        console.log('dentro de selectFromResult',data?.data)
+        return {
+          data: data?.data![0], //una vez recibida la data, la transformamos, y nos quedamos con la etapa del niño
+        };
+      },
       skip: !ChildExists,
     }
   );
