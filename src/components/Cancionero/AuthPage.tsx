4cf1037fdcb83273c4ab9aef7b6c6f733c6b4649
unknown <joakig6@gmail.com>
2023-12-05 01:14:22 -0300

    cancionero page, refactor code

--- src/components/Cancionero/AuthPage.tsx
+++ src/components/Cancionero/AuthPage.tsx
@@ -1,7 +1,44 @@
+import { useGetPlayListQuery } from "@/store/apis/CantajuegaApi";
+import MusicsPlayers from "../MusicPlayer/MusicsPlayers";
+import NormalPage from "./NormalPage";
+import bg from "../../../public/img/Untitled_Artwork-3-1.png";
+
+import Image from "next/image";
+import { IoMdDownload } from "react-icons/io";
+
 export default function AuthPage() {
-    return (
-        <div>
-            <h1>AuthPage</h1>
+  const { isLoading, data: playList, isError } = useGetPlayListQuery(null);
+  return (
+    <div className="flex md:flex-row flex-col h-screen  ">
+      <section className=" w-full md:w-6/12 max-h-[20rem] md:max-h-full flex flex-col ">
+        <MusicsPlayers Musics={playList?.Music!} />
+      </section>
+
+      <section className=" w-full  md:w-6/12 flex flex-col items-center h-full justify-between  ">
+        <p className="text-center p-4 md:text-lg text-base">
+          Nuestro cancionero es una colección de canciones únicas y originales
+          creadas por nuestro talentoso equipo de cantantes y nuestro productor
+          musical. Cada letra y melodía ha sido escrita específicamente para
+          adaptarse a la etapa de desarrollo en la que se encuentran las niñas y
+          niños, lo que las hace hermosas, divertidas y sobre todo educativas.
+          Nos enorgullece decir que nuestras canciones están llenas de amor y
+          pasión por lo que hacemos, y esperamos que puedan disfrutarlas tanto
+          como nosotros disfrutamos en su creación.
+        </p>
+        <div className=" w-full grow h-full flex flex-col items-center">
+          <Image
+            alt="cancionerobg"
+            src={bg}
+            className=" md:block hidden w-5/12 max-w-[30rem]"
+          />
+          <button className="flex items-center justify-center border-[2px] m-auto bg-[#FFFFFF] border-orangeicons w-5/6 min-w-[15rem] max-w-[18rem]  rounded-lg p-2 ">
+            <span className=" w-8/12">Descargar cancionero</span>
+            <span className=" text-black  w-1/12">
+              <IoMdDownload className=" text-2xl" />
+            </span>
+          </button>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
--- src/components/Cancionero/NormalPage.tsx
+++ src/components/Cancionero/NormalPage.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
+import bg from "../../../public/img/Untitled_Artwork-3-1.png";
+import { BsArrowLeft } from "react-icons/bs";
 export default function NormalPage() {
-    return (
-        <div>
-            <p>Normal Page</p>
-        </div>
-    );
+  return (
+    <div className="flex flex-col  md:flex-row  md:gap-0 gap-8  px-8 py-4 pb-14 ">
+      <section
+        className="w-full  text-center flex items-center text-sm min-[500px]:text-base 
+       sm:text-lg  md:text-xl   md:leading-[2.3rem] md:w-7/12 ">
+        <p>
+          Nuestro cancionero es una colección de canciones únicas y originales
+          creadas por nuestro talentoso equipo de cantantes y nuestro productor
+          musical. Cada letra y melodía ha sido escrita específicamente para
+          adaptarse a la etapa de desarrollo en la que se encuentran las niñas y
+          niños, lo que las hace hermosas, divertidas y sobre todo educativas.
+          Nos enorgullece decir que nuestras canciones están llenas de amor y
+          pasión por lo que hacemos, y esperamos que puedan disfrutarlas tanto
+          como nosotros disfrutamos en su creación.
+        </p>
+      </section>
+      <section className="w-5/12   justify-center items-center md:flex hidden">
+        <Image alt="cancionerobg" src={bg} className=" w-10/12 max-w-[30rem]" />
+      </section>
+
+      <article
+        className={` md:absolute bottom-[0%] left-0  w-full  flex justify-center  text-white`}>
+        <button className="flex items-center justify-center bg-blue w-[20rem] rounded-full text-[#FFFFFF]">
+          <span className=" text-orangeicons w-1/12">
+            <BsArrowLeft />{" "}
+          </span>
+          <span className=" w-10/12">
+            Inscríbete para acceder a nuestro contenido
+          </span>
+        </button>
+      </article>
+    </div>
+  );
 }
