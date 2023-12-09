cc7d2bf490222162d6734ba2b2da6421fa0ceef3
unknown <joakig6@gmail.com>
2023-12-09 20:17:02 -0300

    add typescript type in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,29 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { jwtVerify } from 'jose';
-
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { jwtVerify, JWTPayload } from "jose";
+interface userPayload extends JWTPayload {
+  user: {
+    is_Admin?: boolean;
+  };
+}
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('accesscookie');
-
+  const token = req.cookies.get("accesscookie");
   try {
     if (!token) {
       const url = req.nextUrl.clone(); //clonamos la url(localhost o deploy)
-
-      url.pathname = '/'; ///seteamos la url a donde redirigiremos
+      url.pathname = "/"; ///seteamos la url a donde redirigiremos
       return NextResponse.redirect(url); //redirigimos a url base
     }
 
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    if (req.nextUrl.pathname.startsWith("/dashboard")) {
       const { payload } = await jwtVerify(
         token.value,
-        new TextEncoder().encode('SECRETO')
+        new TextEncoder().encode("SECRETO")
       );
-      if (!payload?.user?.is_Admin) {
+      const userPayload = payload as userPayload;
+      if (!userPayload?.user.is_Admin) {
         const url = req.nextUrl.clone();
-        url.pathname = '/';
+        url.pathname = "/";
         return NextResponse.error();
         //Si la ruta visitada es dashboard y no es admin tiro un error el cual lo redirige a la pagina de notfound
       }
@@ -37,5 +40,5 @@ export async function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/mis-cursos', '/dashboard/:path*'],
+  matcher: ["/mis-cursos", "/dashboard/:path*"],
 };
