51c336a0032c7a9db13fb7e4cdaf2833030ae5d4
unknown <joakig6@gmail.com>
2023-12-15 11:33:00 -0300

    some changes

--- src/app/(sharedLayout)/mis-cursos/page.tsx
+++ src/app/(sharedLayout)/mis-cursos/page.tsx
@@ -1,51 +1,56 @@
 "use client";
 import styles from "../../../styles/Miscursos.module.css";
-import { useAuthQuery,useGetChildQuery, useGetStageQuery ,useGetProgressChildQuery} from "@/store/apis/CantajuegaApi";
+import {
+  useAuthQuery,
+  useGetChildQuery,
+  useGetStageQuery,
+  useGetProgressChildQuery,
+} from "@/store/apis/CantajuegaApi";
 
 import MisCursosContent from "@/components/MisCursos/MisCursosContent";
 import { useAppSelector } from "@/store/hooks";
 
 export default function Miscursos() {
-  const { data, isLoading, isError, isSuccess } = useAuthQuery(null,{
-    refetchOnMountOrArgChange:true,
+  const { data, isLoading, isError, isSuccess } = useAuthQuery(null, {
+    refetchOnMountOrArgChange: true,
   });
   const Child = useAppSelector((state) => state.childReducer.child);
+  const User = useAppSelector((state) => state.userReducer.user);
+
   const ChildExist = Child?.StageId ? true : false;
-  
-  const { stage } = useGetStageQuery({childs:null}, {
-    selectFromResult: ({ data }) => ({
-      stage: data?.data?.filter((item) => item.id === Child?.StageId)[0], //una vez recibida la data, la transformamos, y nos quedamos con la etapa del niño
-    }),
-    skip: !ChildExist,
-  });
-  const {data:getProgress}=useGetProgressChildQuery({ProgressId:Child?.ProgressId as string},{
-    skip:!ChildExist,
-    refetchOnFocus:true
-  })
-  
-  if(isError){
-    return (
-      <div className="min-h-screen flex justify-center items-center">
-        <h1>Inicia sesion o registrate.</h1>
-      </div>
-    )
-  }
-  if (isLoading) {
-    return (
-      <div className="min-h-screen flex justify-center items-center">
-        <h1>Cargando....</h1>;
-      </div>
-    );
-  }
-  console.log(ChildExist,Child,'en componente')
+
+  const { stage } = useGetStageQuery(
+    { childs: null },
+    {
+      selectFromResult: ({ data }) => ({
+        stage: data?.data?.filter((item) => item.id === Child?.StageId)[0], //una vez recibida la data, la transformamos, y nos quedamos con la etapa del niño
+      }),
+      skip: !ChildExist,
+    }
+  );
+  const { data: getProgress } = useGetProgressChildQuery(
+    { ProgressId: Child?.ProgressId as string },
+    {
+      skip: !ChildExist,
+      refetchOnFocus: true,
+    }
+  );
 
   return (
     <div
       id="MisCursosPage"
       className={`${styles.Container} flex justify-between bg-white min-h-screen `}>
-  
-        <MisCursosContent Stage={stage} ChildExists={ChildExist} ProgressId={Child?.ProgressId}/>
-
+      {User?.Membership && (
+        <MisCursosContent
+          Stage={stage}
+          ChildExists={ChildExist}
+          ProgressId={Child?.ProgressId}
+        />
+      )}
+      {!User?.Membership && <div className="flex justify-center items-center">
+        
+         <h1 className= "text-3xl">Actualmente no tienes ninguna membresia adquirida</h1>
+         </div>}
     </div>
   );
 }
