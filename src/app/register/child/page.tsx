37b216a46c9241e6f31dc368ba2aff179cb70964
unknown <joakig6@gmail.com>
2023-12-16 13:55:15 -0300

    complete second step in  google register

--- src/app/register/child/page.tsx
+++ src/app/register/child/page.tsx
@@ -1,30 +1,23 @@
-'use client';
-import styles from '../../../styles/Register.module.css';
-import Image from 'next/image';
-import img1 from '../../../../public/img/login/Ellipse 15.png';
-import img2 from '../../../../public/img/login/Ellipse 17.png';
-import img3 from '../../../../public/img/login/Ellipse 18.png';
-import img4 from '../../../../public/img/login/Ellipse 19.png';
-import { GoAlert } from 'react-icons/go';
-import { RotatingLines } from 'react-loader-spinner';
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+"use client";
+import styles from "../../../styles/register.module.css";
+import Image from "next/image";
+import img1 from "../../../../public/img/login/Ellipse 15.png";
+import img2 from "../../../../public/img/login/Ellipse 17.png";
+import img3 from "../../../../public/img/login/Ellipse 18.png";
+import img4 from "../../../../public/img/login/Ellipse 19.png";
+import { GoAlert } from "react-icons/go";
+import { RotatingLines } from "react-loader-spinner";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import {
   InputChilds,
   inputErrorChilds,
   registerErrorChild,
-  registerErrorUser,
-} from '@/utils/FormsErrors';
-import { registerUser } from '@/libs/functions';
-import { useRouter } from 'next/navigation';
-import AcountConfirmation from '@/components/AcountConfirmation/AcountConfirmation';
-import StepTwo from '@/components/Register/StepTwo';
+} from "@/utils/FormsErrors";
+import { CompleteChildRegister } from "@/libs/functions";
+import { useRouter } from "next/navigation";
+import StepTwo from "@/components/Register/StepTwo";
+import Cookies from "js-cookie";
 
-interface IRegister {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-}
 interface IRegisterErrors {
   firstName?: string;
   lastName?: string;
@@ -37,57 +30,63 @@ export default function RegisterChild() {
   const router = useRouter();
   const [errors, setErrors] = useState<IRegisterErrors>({});
   const [errorsChild, setErrorsChild] = useState<inputErrorChilds>({});
+  const [temporalUser, setTemporalUser] = useState({
+    firstName: "",
+  });
   const [childData, setChildData] = useState<InputChilds>({
-    firstName: '',
-    lastName: '',
-    birthDate: '',
-    gender: 'male',
+    firstName: "",
+    lastName: "",
+    birthDate: "",
+    gender: "male",
   });
-  const [registerValues, setRegisterValues] = useState<IRegister>({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-  });
+
+  useEffect(() => {
+    const cookie = Cookies.get("register");
+    const object = JSON.parse(cookie || "{}");
+    const user = {
+      firstName: object.user,
+    };
+    setTemporalUser(user);
+
+    () => Cookies.remove("register");
+  }, []);
 
   const [loading, setLoading] = useState(false);
 
   const handleInputChange = (
     event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-    type: 'user' | 'child'
+    type: "user" | "child"
   ): void => {
     const { name, value } = event.target;
-    if (type === 'user') {
-      setRegisterValues({ ...registerValues, [name]: value });
-    } else {
-      setChildData({ ...childData, [name]: value });
-    }
+
+    setChildData({ ...childData, [name]: value });
   };
 
   const [verificationModal, setVerificationModal] = useState<boolean>(false);
   const handleSubmit = async (
     event: FormEvent<HTMLFormElement>
   ): Promise<void> => {
+    console.log("me ejecuto");
     event.preventDefault();
-    const { errors, anyError } = registerErrorUser(registerValues);
     const { errors: childErrors, anyError: anyErrorChild } =
       registerErrorChild(childData);
     setErrorsChild(childErrors);
-    console.log(childData);
-    if (!anyError && !anyErrorChild) {
+    if (!anyErrorChild) {
       try {
         setLoading(true);
-        await registerUser({ user: registerValues, child: childData });
+
+        console.log(childData);
+        await CompleteChildRegister(childData);
         setLoading(false);
         setVerificationModal(true);
-        // router.push('/');
+        router.push('/');
       } catch (error: any) {
         setErrors({
           ...errors,
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: '',
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
           global: error.message,
         });
         setLoading(false);
@@ -96,18 +95,14 @@ export default function RegisterChild() {
   };
   return (
     <div className={styles.container}>
-      {verificationModal && (
-        <AcountConfirmation
-          email={registerValues.email}
-          name={registerValues.firstName}
-        />
-      )}
-      <Image className={styles.img1} src={img1} alt='decoration1' />
-      <Image className={styles.img2} src={img2} alt='decoration2' />
-      <Image className={styles.img3} src={img3} alt='decoration3' />
-      <Image className={styles.img4} src={img4} alt='decoration4' />
+      <Image className={styles.img1} src={img1} alt="decoration1" />
+      <Image className={styles.img2} src={img2} alt="decoration2" />
+      <Image className={styles.img3} src={img3} alt="decoration3" />
+      <Image className={styles.img4} src={img4} alt="decoration4" />
       <div className={styles.tittleContainer}>
-        <h2 className={styles.tittle}>Datos del niño/niña</h2>
+        <h2 className={`${styles.tittle} text-white`}>
+          Hola {temporalUser.firstName}
+        </h2>
       </div>
 
       <form className={styles.form} onSubmit={handleSubmit}>
@@ -121,12 +116,12 @@ export default function RegisterChild() {
           {loading && (
             <div className={styles.spinnerContainer}>
               <RotatingLines
-                strokeColor='#ff3d00'
-                strokeWidth='5'
-                animationDuration='0.75'
-                width='30'
+                strokeColor="#ff3d00"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="30"
                 visible={true}
-              />{' '}
+              />{" "}
               Verificando cuenta...
             </div>
           )}
